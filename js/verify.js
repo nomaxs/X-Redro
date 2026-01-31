07a85de5eb1c9ec4d47d1d0e2fc30a2df92e2ba4
nomaxs <110211638+nomaxs@users.noreply.github.com>
2026-01-31 17:39:04 +0100

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -4,7 +4,7 @@ async function confirmVerification() {
   const secret = params.get("secret");
 
   if (!userId || !secret) {
-    alert("Invalid verification link");
+    showToast("Invalid verification link", "error");
     return;
   }
 
